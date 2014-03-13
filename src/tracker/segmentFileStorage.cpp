c0a444f1eb155795d27e0fd15617693a2ea90a8a
salivan-ratcoin-dev-team <salivan.ratcoin.dev.team@gmail.com>
2014-03-13 13:57:47 +0100

    further progress in segment file storage

--- src/tracker/segmentFileStorage.cpp
+++ src/tracker/segmentFileStorage.cpp
@@ -33,9 +33,17 @@ CSegmentFileStorage::CSegmentFileStorage()
 
 struct CTransactionRecord
 {
-	static unsigned int const ms_defaultLevel = 4;
+	static unsigned int const ms_buddyBaseLevel = 16;
+
+	static unsigned int const ms_buddySize = 1 << ( KiloByteShift* 512); // in bytes
 
 	CTransactionRecord();
+	
+	static unsigned int getBuddyLevel( size_t _transactionSize );
+
+	static size_t getBuddySize( unsigned int  _level );
+
+	void * translateToAddress( unsigned int _index );
 
 	CSimpleBuddy m_simpleBuddy;
 
@@ -43,11 +51,46 @@ struct CTransactionRecord
     (
         READWRITE(m_simpleBuddy);
     )
-
 };
 
+unsigned int
+CTransactionRecord::getBuddyLevel( size_t const _transactionSize )
+{
+	size_t baseUnit = ms_buddySize >> ms_buddyBaseLevel;
+
+	unsigned int level = ms_buddyBaseLevel;
+
+	while( baseUnit < _transactionSize && level )
+	{
+		level--;
+		baseUnit <<=2;
+	}
+
+	if ( baseUnit < _transactionSize )
+	{
+		throw std::exception();
+	}
+
+	return level;
+}
+
+size_t
+CTransactionRecord::getBuddySize( unsigned int  _level )
+{
+	size_t baseUnit = ms_buddySize >> ms_buddyBaseLevel;
+
+	return baseUnit << _level;
+}
+
+void * 
+CTransactionRecord::translateToAddress( unsigned int _index )
+{
+	size_t baseUnit = ms_buddySize >> ms_buddyBaseLevel;
+	return m_simpleBuddy.m_area[ _index ];
+}
+
 CTransactionRecord::CTransactionRecord()
-	: m_simpleBuddy(ms_defaultLevel)
+	: m_simpleBuddy(ms_buddyBaseLevel)
 {
 }
 
@@ -71,19 +114,19 @@ private:
 
 
 void
-CHeader::increaseUsageRecord( unsigned int _recordId )
+CSegmentHeader::increaseUsageRecord( unsigned int _recordId )
 {
 
 }
 
 void
-CHeader::decreaseUsageRecord( unsigned int _recordId )
+CSegmentHeader::decreaseUsageRecord( unsigned int _recordId )
 {
 
 }
 
 bool
-CHeader::setNewRecord( unsigned int _bucked, CRecord const & _record )
+CSegmentHeader::setNewRecord( unsigned int _bucked, CRecord const & _record )
 {
 	unsigned int maxNumber =  m_recordsNumber / m_maxBucket;
 
@@ -101,28 +144,28 @@ CHeader::setNewRecord( unsigned int _bucked, CRecord const & _record )
 
 inline
 IndicatorType
-CHeader::getNextHeader() const
+CSegmentHeader::getNextHeader() const
 {
 	return m_nextHeader;
 }
 
 inline
 void
-CHeader::setNextHeader( IndicatorType _nextHeader )
+CSegmentHeader::setNextHeader( IndicatorType _nextHeader )
 {
 	m_nextHeader = _nextHeader;
 }
 
 inline
 bool
-CHeader::givenRecordUsed(unsigned int _index )
+CSegmentHeader::givenRecordUsed(unsigned int _index )
 {
 	return m_records[_index].m_blockNumber;
 }
 
 inline
 unsigned int const
-CHeader::getRecordNumber()
+CSegmentHeader::getRecordNumber()
 {
 	return m_recordsNumber;
 }
@@ -141,10 +184,36 @@ CSegmentFileStorage::includeTransactions( std::list< CTransaction > const & _tra
 	m_transactionsToStore.insert (m_transactionsToStore.end(),_transaction.begin(),_transaction.end());
 }
 
+void 
+CSegmentFileStorage::flushLoop()
+{
+	while(1)
+	{
+		{
+			boost::lock_guard<boost::mutex> lock(m_lock);
+
+				ToInclude = m_discCache.equal_range(bucked);
+
+						for ( CacheIterators iterator=ToInclude.first; iterator!=ToInclude.second; ++iterator )
+
+					CTransactionRecord transactionRecord;
+					
+					// this  code is executed during  flushes
+					/*if ( header.setNewRecord( bucket, transactionRecord ) );
+					{
+						header = createNewHeader();
+						header = setNewRecord( bucket, transactionRecord )
+					}*/
+
+		boost::this_thread::interruption_point();
+	}
+}
+
 void
 CSegmentFileStorage::loop()
 {
-	FILE* file = OpenDiskFile(CDiskBlockPos(0,0), ms_fileName.c_str(), true);
+	CSegmentHeader header = findLastHeader();
+
 	while(1)
 	{
 		{
@@ -153,34 +222,72 @@ CSegmentFileStorage::loop()
 			std::list< CTransaction >::iterator iterator = m_transactionsToStore.begin();
 			while(iterator != m_transactionsToStore.end())
 			{
-				iterator->GetHash();
+				unsigned int bucket = calculateBucket( iterator->GetHash() );
+				unsigned int reqLevel = CTransactionRecord::getBuddyLevel( iterator->GetSerializeSize(SER_DISK, CTransaction::CURRENT_VERSION) );
 
+				ToInclude toInclude;
+
+				toInclude = m_discCache.equal_range(bucked);
+
+				int index = -1;
+				if ( toInclude.first != m_discCache.end() )
+				{
+					for ( CacheIterators cacheIterator=toInclude.first; cacheIterator!=toInclude.second; ++cacheIterator )
+					{
+						index = cacheIterator->buddyAlloc( reqLevel );
+						if ( index == -1 )
+							continue;
+						else
+						{
+							CBufferAsStream stream(
+								cacheIterator->translateToAddress( index )
+								, cacheIterator->getBuddySize( reqLevel )
+								, SER_DISK
+								, CLIENT_VERSION);
+
+							stream << *iterator;
+
+							break;
+						}
+					}
+				}
+
+				if ( index == -1 )
+				{
+					CTransactionRecord transactionRecord;
+					
+					CBufferAsStream stream(
+						  transactionRecord.translateToAddress( index )
+						, transactionRecord.getBuddySize( reqLevel )
+						, SER_DISK
+						, CLIENT_VERSION);
+
+					stream << *iterator;
+
+					m_discCache.insert( std::make_pair ( bucket,transactionRecord); )
+				}
 				iterator++;
 			}
 		}
 		boost::this_thread::interruption_point();
-
 	}
-
 }
 
-
-
-CHeader *
+CSegmentHeader *
 CSegmentFileStorage::createNewHeader()
 {
 	void * nextBlock = getNextFreeBlock();
 	if ( !nextBlock )
 		return 0;
 
-	CHeader * lastHeader = static_cast< CHeader * >( getBlock( findLastHeader() ) );
+	CSegmentHeader * lastHeader = static_cast< CSegmentHeader * >( getBlock( findLastHeader() ) );
 
 	if ( !lastHeader )
 		return 0;
 
 	lastHeader->setNextHeader( calculateBlockIndex ( nextBlock ) );
 
-	return static_cast< CHeader * >( nextBlock );
+	return static_cast< CSegmentHeader * >( nextBlock );
 }
 
 void
@@ -189,11 +296,11 @@ CSegmentFileStorage::readTransactions( CCoinsViewCache * _coinsViewCache )
 	FILE* file = OpenDiskFile(CDiskBlockPos(0,0), ms_fileName.c_str(), true);
 
 
-	CHeader header;
+	CSegmentHeader header;
 	CBufferedFile blkdat(file, 2*MAX_BLOCK_SIZE, MAX_BLOCK_SIZE+8, SER_DISK, CLIENT_VERSION);
 	blkdat >> header;
 // it  is not  full  only some  experimental  crap
-	for ( unsigned i = 0; i < CHeader::getRecordNumber(); ++i )
+	for ( unsigned i = 0; i < CSegmentHeader::getRecordNumber(); ++i )
 	{
 		CTransactionRecord transactionRecord;
 		if ( header.givenRecordUsed(i))
@@ -220,11 +327,26 @@ CSegmentFileStorage::readTransactions( CCoinsViewCache * _coinsViewCache )
 		}
 	}
 }
-
+// slow
 void
 CSegmentFileStorage::eraseTransaction( CTransaction const & _transaction )
 {
+}
 
+void
+CSegmentFileStorage::eraseTransaction( CCoins const & _coins )
+{
+	
+	if ( _coins.nHeight )
+	{
+
+	}
+	else
+	{
+		_coins.m_bucket
+	}
+
+		_transaction.
 }
 
 
@@ -240,16 +362,16 @@ CSegmentFileStorage::getNextFreeBlock()
 	return 0;
 }
 
-void *
-CSegmentFileStorage::getBlock( unsigned int _index )
+CSegmentHeader
+CSegmentFileStorage::findLastHeader(CBufferedFile const & blkdat)
 {
-	return 0;
-}
-
-unsigned int
-CSegmentFileStorage::findLastHeader()
-{
-	return 0;
+	CSegmentHeader header = getBlock<CSegmentHeader>( 0 );
+	
+	while(header.getNextHeader())
+	{
+		header = getBlock<CSegmentHeader>( header.getNextHeader() )
+	}
+	return header;
 }
 
 unsigned int
--- src/tracker/segmentFileStorage.h
+++ src/tracker/segmentFileStorage.h
@@ -11,6 +11,8 @@
 #include "uint256.h"
 #include "serialize.h"
 
+#define  KiloByteShift 10
+
 class CTransaction;
 
 class CCoinsViewCache;
@@ -22,8 +24,6 @@ namespace self
 #define TRANSACTION_MAX_SIZE ( 1 << 8 )
 #define MAX_BUCKET ( 0xff -1 )
 
-
-typedef unsigned int IndicatorType;
 typedef unsigned int CounterType;
 
 //::GetSerializeSize(tx, SER_NETWORK, PROTOCOL_VERSION);
@@ -32,7 +32,7 @@ typedef unsigned int CounterType;
 struct CRecord
 {
 
-	IndicatorType m_blockNumber;
+	unsigned int m_blockNumber;
 	char m_isEmptySpace;
 
     IMPLEMENT_SERIALIZE
@@ -42,7 +42,7 @@ struct CRecord
     )
 };
 
-class CHeader
+class CSegmentHeader
 {
 public:
 	void increaseUsageRecord( unsigned int _recordId );
@@ -57,17 +57,17 @@ public:
         READWRITE(m_headerHash);
     )
 
-	IndicatorType getNextHeader() const;
-	void setNextHeader( IndicatorType _nextHeader );
+	unsigned int getNextHeader() const;
+	void setNextHeader( unsigned int _nextHeader );
 
 	static unsigned int const getRecordNumber();
 
 	bool givenRecordUsed(unsigned int _index );
 private:
-	static unsigned int const  m_recordsNumber =  ( BLOCK_SIZE - sizeof( IndicatorType )*2 -  sizeof( uint256 ) )/ sizeof( CRecord );
+	static unsigned int const  m_recordsNumber =  ( BLOCK_SIZE - sizeof( unsigned int )*2 -  sizeof( uint256 ) )/ sizeof( CRecord );
 	static unsigned int const  m_maxBucket = MAX_BUCKET;
 
-	IndicatorType m_nextHeader;
+	unsigned int m_nextHeader;
 	CRecord m_records[ m_recordsNumber ];
 
 	uint256 m_headerHash;
@@ -87,28 +87,50 @@ public:
 	void readTransactions( CCoinsViewCache * _coinsViewCache );
 
 	void loop();
+	
+	void flushLoop()
 private:
-	CHeader * createNewHeader();
+	CSegmentHeader createNewHeader();
 
 	unsigned int calculateBucket( uint256 const & _coinsHash ) const;
 
 	void * getNextFreeBlock();
 
-	void * getBlock( unsigned int _index );
+	template< class T >
+	T getBlock( unsigned int _index );
 
-	unsigned int findLastHeader();
+	CSegmentHeader findLastHeader();
 
 	unsigned int calculateBlockIndex( void * _block );
-
-	static const std::string ms_fileName;
 private:
+	typedef std::multimap< unsigned int,CDiskBlock >::iterator CacheIterators;
+
+	typedef std::pair< CacheIterators, CacheIterators > ToInclude;
+private:
+	static const std::string ms_fileName;
 
 	mutable boost::mutex m_lock;
 
 	std::list< CTransaction > m_transactionsToStore;
+
+	mutable boost::mutex m_cachelock;
+	std::multimap< unsigned int,CDiskBlock > m_discCache;
+
+	static size_t const m_segmentSize = 1 << KiloByteShift * 512;
 };
 
+template< class T >
+T 
+CSegmentFileStorage::getBlock( unsigned int _index )
+{
+	FILE* file = OpenDiskFile(CDiskBlockPos( 0,m_segmentSize * _index ), ms_fileName.c_str(), true);
+	CBufferedFile blkdat(file, 2*m_segmentSize, m_segmentSize+8, SER_DISK, CLIENT_VERSION);
 
+	T block;
+	blkdat >> block;
+
+	return block;
+}
 
 }
 
