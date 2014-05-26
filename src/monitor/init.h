58c437ef5e60db65882db6981819318852b02cdb
Salivan Atue <salivan.ratcoin.dev.team@gmail.com>
2014-05-26 17:10:11 +0100

    add init  to monitor

---
+++ src/monitor/init.h
@@ -0,0 +1,22 @@
+// Copyright (c) 2014 Ratcoin dev-team
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef INIT_H
+#define INIT_H
+
+#include <boost/thread.hpp>
+
+class CWallet;
+
+extern std::string strWalletFile;
+extern CWallet* pwalletMain;
+
+namespace monitor
+{
+
+bool AppInit(boost::thread_group& threadGroup);
+void Shutdown();
+}
+
+#endif // INIT_H
---
+++ src/monitor/processNetwork.h
@@ -0,0 +1,14 @@
+#ifndef PROCESS_NETWORK_H
+#define PROCESS_NETWORK_H
+
+namespace monitor
+{
+
+class CProcessNetwork
+{
+
+};
+
+}
+
+#endif // PROCESS_NETWORK_H
