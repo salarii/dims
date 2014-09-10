f098bc2ffc6ce7c3f33f66709db3f4ef3e0dc257
Salivan Atue <salivan.ratcoin.dev.team@gmail.com>
2014-09-10 22:58:15 +0100

    fix previous

--- src/txdb.h
+++ src/txdb.h
@@ -43,28 +43,4 @@ public:
 	void clearView();
 };
 
-/** Access to the block database (blocks/index/) */
-class CBlockTreeDB : public CLevelDBWrapper
-{
-public:
-    CBlockTreeDB(size_t nCacheSize, bool fMemory = false, bool fWipe = false);
-private:
-    CBlockTreeDB(const CBlockTreeDB&);
-    void operator=(const CBlockTreeDB&);
-public:
-    bool WriteBlockIndex(const CDiskBlockIndex& blockindex);
-    bool WriteBestInvalidWork(const CBigNum& bnBestInvalidWork);
-    bool ReadBlockFileInfo(int nFile, CBlockFileInfo &fileinfo);
-    bool WriteBlockFileInfo(int nFile, const CBlockFileInfo &fileinfo);
-    bool ReadLastBlockFile(int &nFile);
-    bool WriteLastBlockFile(int nFile);
-    bool WriteReindexing(bool fReindex);
-    bool ReadReindexing(bool &fReindex);
-    bool ReadTxIndex(const uint256 &txid, CDiskTxPos &pos);
-    bool WriteTxIndex(const std::vector<std::pair<uint256, CDiskTxPos> > &list);
-    bool WriteFlag(const std::string &name, bool fValue);
-    bool ReadFlag(const std::string &name, bool &fValue);
-    bool LoadBlockIndexGuts();
-};
-
 #endif // BITCOIN_TXDB_LEVELDB_H
