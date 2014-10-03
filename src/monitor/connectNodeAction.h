78702f541ff0395f99722989c808427154ec5cd8
Salivan Atue <salivan.ratcoin.dev.team@gmail.com>
2014-10-03 20:45:52 +0100

    add connect  node action

---
+++ src/monitor/connectNodeAction.h
@@ -0,0 +1,80 @@
+// Copyright (c) 2014 Dims dev-team
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef CONNECT_NODE_ACTION_H
+#define CONNECT_NODE_ACTION_H
+
+#include "common/action.h"
+#include "common/filters.h"
+
+#include "configureMonitorActionHandler.h"
+
+#include <boost/statechart/state_machine.hpp>
+
+#include "protocol.h"
+/*
+current communication protocol is ineffective
+consider using ack  request after  every successful message  reception
+*/
+
+
+namespace monitor
+{
+
+struct CMonitorConnectNodeActionUninitiated;
+
+class CConnectNodeAction : public common::CAction< MonitorResponses >, public  boost::statechart::state_machine< CConnectNodeAction, CMonitorConnectNodeActionUninitiated >, public common::CCommunicationAction
+{
+public:
+	CConnectNodeAction( std::string const & _nodeAddress );
+
+	CConnectNodeAction( CAddress const & _addrConnect );
+
+	CConnectNodeAction( uint256 const & _actionKey, std::vector< unsigned char > const & _payload, uintptr_t _mediumPtr );
+
+	virtual common::CRequest< MonitorResponses >* execute();
+
+	virtual void accept( common::CSetResponseVisitor< MonitorResponses > & _visitor );
+
+	void setRequest( common::CRequest< MonitorResponses >* _request );
+
+	common::CRequest< MonitorResponses > const * getRequest() const;
+
+	std::string getAddress() const;
+
+	CAddress getServiceAddress() const;
+
+	std::vector< unsigned char > const & getPayload() const;
+
+	void setMediumPtr( uintptr_t _mediumPtr );
+
+	uintptr_t getMediumPtr() const;
+
+	CPubKey getPublicKey() const;
+
+	void setPublicKey( CPubKey const & _pubKey );
+
+	~CConnectNodeAction(){};
+private:
+	common::CRequest< MonitorResponses >* m_request;
+
+	std::string const m_nodeAddress;
+
+	static int const ms_randomPayloadLenght = 32;
+
+	std::vector< unsigned char > m_payload;
+
+	bool const m_passive;
+
+	CAddress m_addrConnect;
+
+	CPubKey m_key;
+
+	uintptr_t m_mediumPtr;
+};
+
+
+}
+
+#endif // CONNECT_NODE_ACTION_H
--- src/monitor/setResponseVisitor.cpp
+++ src/monitor/setResponseVisitor.cpp
@@ -1,1 +1,77 @@
+// Copyright (c) 2014 Dims dev-team
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include "common/setResponseVisitor.h"
+#include "common/responseVisitorInternal.h"
+#include "common/commonEvents.h"
+#include "connectNodeAction.h"
+
+namespace common
+{
+
+template < class _Action >
+class CSetNodeConnectedResult : public CResponseVisitorBase< _Action, monitor::MonitorResponseList >
+{
+public:
+	CSetNodeConnectedResult( _Action * const _action ):CResponseVisitorBase< _Action, monitor::MonitorResponseList >( _action ){};
+
+	virtual void operator()( common::CConnectedNode & _param ) const
+	{
+		if ( _param.m_node )
+			this->m_action->process_event( common::CNodeConnectedEvent( _param.m_node ) );
+		else
+			this->m_action->process_event( common::CCantReachNode() );
+	}
+
+	virtual void operator()( common::CIdentificationResult & _param ) const
+	{
+		this->m_action->process_event( common::CIntroduceEvent( _param.m_payload, _param.m_signed, _param.m_key, _param.m_address) );
+	}
+
+	virtual void operator()( common::CContinueResult & _param ) const
+	{
+		this->m_action->process_event( common::CContinueEvent( _param.m_id ) );
+	}
+
+	virtual void operator()( common::CRoleResult & _param ) const
+	{
+		this->m_action->process_event( common::CRoleEvent( _param.m_role ) );
+	}
+
+	virtual void operator()( common::CNetworkInfoResult & _param ) const
+	{
+		this->m_action->process_event( common::CNetworkInfoEvent( _param.m_networkInfo ) );
+	}
+
+	virtual void operator()( common::CAckPromptResult & _param ) const
+	{
+		this->m_action->process_event( _param );
+	}
+
+	virtual void operator()( common::CAckResult & _param ) const
+	{
+		this->m_action->process_event( common::CAckEvent() );
+	}
+
+	virtual void operator()( common::CGetPrompt & _param ) const
+	{
+		this->m_action->process_event( common::CGetEvent(_param.m_type) );
+	}
+};
+
+
+CSetResponseVisitor< monitor::MonitorResponses >::CSetResponseVisitor( monitor::MonitorResponses const & _requestResponse )
+	: m_requestResponse( _requestResponse )
+{
+}
+
+void
+CSetResponseVisitor< monitor::MonitorResponses >::visit( monitor::CConnectNodeAction & _action )
+{
+	boost::apply_visitor( (CResponseVisitorBase< monitor::CConnectNodeAction, monitor::MonitorResponseList > const &)CSetNodeConnectedResult< monitor::CConnectNodeAction >( &_action ), m_requestResponse );
+}
+
+
+}
 
