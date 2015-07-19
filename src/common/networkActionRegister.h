caff02f267f587a1e9ea153bf1627253a32f68ea
Salivan Atue <salivan.ratcoin.dev.team@gmail.com>
2015-07-19 19:05:06 +0100

    add network action files

--- src/common/networkActionRegister.h
+++ src/common/networkActionRegister.h
@@ -1,4 +1,31 @@
-#ifndef NETWORKACTIONREGISTER_H
-#define NETWORKACTIONREGISTER_H
+// Copyright (c) 2014-2015 Dims dev-team
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#endif // NETWORKACTIONREGISTER_H
+#ifndef NETWORK_ACTION_REGISTER_H
+#define NETWORK_ACTION_REGISTER_H
+
+#include "util.h"
+
+namespace common
+{
+
+class CNetworkActionRegister
+{
+public:
+	static CNetworkActionRegister* getInstance( );
+
+	void registerServicedByAction( uint256 const & _actionKey );
+
+	void unregisterServicedByAction( uint256 const & _actionKey );
+
+	bool isServicedByAction( uint256 const & _actionKey ) const;
+private:
+	static CNetworkActionRegister * ms_instance;
+
+	std::set< uint256 > m_actionsInProgress;
+};
+
+}
+
+#endif // NETWORK_ACTION_REGISTER_H
